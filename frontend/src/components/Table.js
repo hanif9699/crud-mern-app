77ce8d1265f88b023b3f27b50bbd27c39b5d11d1
Badsha Hanif <badsha@paperplane.net>
2021-09-15 10:12:07 +0530

    Done

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -22,10 +22,10 @@ const Table = () => {
             selector: row => row.image,
             cell:row => { return (<div className="img-wrapper"><img src={row.image} alt={row.id} /></div>)}
         },
-        {
-            name: 'Edit Action',
-            cell:row => { return (<div className="button-wrapper"><button type="button" className="btn-custom edit-btn">Edit</button></div>)}
-        },
+        // {
+        //     name: 'Edit Action',
+        //     cell:row => { return (<div className="button-wrapper"><button type="button" className="btn-custom edit-btn">Edit</button></div>)}
+        // },
         {
             name: 'Delete Action',
             cell:row => { return (<div className="button-wrapper"><button type="button" className="btn-custom delete-btn" onClick={()=>{dispatch(deleteDetails({id:row.id}))}}>Delete</button></div>)}
